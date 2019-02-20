b081c912b2b2b337220bf5b9e4c3491dbb0c5583
Lasse <lassebirkegaard91@gmail.com>
2019-02-20 11:38:36 +0100

    Version 2

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"})
-    response.end("Hello you\n");
+    response.end("Hello you updated version now! \n");
 });
 
 server.listen(8000);
