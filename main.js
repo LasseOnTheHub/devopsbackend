607aaedbacfe2c9f85c30de961770730f5eaa8e5
Lasse <lassebirkegaard91@gmail.com>
2019-02-20 10:42:53 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+var server = http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/plain"})
+    response.end("Hello you\n");
+});
+
+server.listen(8000);
+console.log("Backend started on port 8000")
